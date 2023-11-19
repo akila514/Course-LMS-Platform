c94840a18eb1f1f10dcd704eeb2142495c53677d
akila514 <akilajay514@gmail.com>
2023-11-19 09:23:54 +0530

    Chapter can be published

--- lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -24,6 +24,32 @@ const ChapterActions = ({
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/unpublish`
+        );
+        toast.success("Chapter unpublished ");
+      }
+
+      if (!isPublished) {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/publish`
+        );
+        toast.success("Chapter published ");
+      }
+
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const onDelete = async () => {
     try {
       setIsLoading(true);
@@ -43,7 +69,7 @@ const ChapterActions = ({
   return (
     <div className="flex items-center gap-x-2">
       <Button
-        onClick={onDelete}
+        onClick={onClick}
         disabled={disabled || isLoading}
         variant="outline"
         size="sm"
